ede5397128f103033d85a78cf57aac403549aa44
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-03 14:02:34 -0400

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Even or Odd");
+Console.Write("Enter an integer: ");
+var answer = Console.ReadLine();
+var input = Convert.ToInt32(answer);
+
+while(input != 0)
+{
+    if (input % 2 == 0)
+    {
+        Console.WriteLine(answer + " is even");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine(answer + " is odd");
+        Console.WriteLine();
+    }
+
+    Console.Write("Enter an integer: ");
+    answer = Console.ReadLine();
+    input = Convert.ToInt32(answer);
+}
+
+Console.WriteLine("Done...");
